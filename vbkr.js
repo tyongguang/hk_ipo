64b626c86d5d1453ea8a383588f799bfff181ecc
wikotan <wikotan@tencent.com>
2021-02-01 21:07:22 +0800

    使用新框架

--- vbkr.js
+++ vbkr.js
@@ -12,29 +12,50 @@ subscription_node = '//*[@id="iposVue"]/div[2]/div[2]/div[2]/div[1]/ul[1]/li[2]/
 select_node = '//*[@id="iposVue"]/div[2]/div[2]/div[2]/div[1]/ul[1]/li[3]/span[2]/div/div';
 buy_node = '//*[@id="iposVue"]/div[2]/div[2]/div[2]/a';
 
-subscription_ele = getElementByXpath(subscription_node);
-if (subscription_ele == null) {
-     location.reload();
-}
 
-subscription_ele.click();
-sleep(200).then(() => {
-     getElementByXpath(select_node).click();
-     sleep(300).then(() => {
+var ary = new Array()
+ary.push(
+     100, function () {
+          subscription_ele = getElementByXpath(subscription_node);
+          if (subscription_ele == null) {
+               location.reload();
+          }
+     },
+     200, function () {
+          getElementByXpath(select_node).click();
+     },
+     300, function () {
           cnt_ele = getElementByXpath(cnt);
           if (cnt_ele.className == 'option-item' || cnt_ele.className == 'option-item red') {
                getElementByXpath(cnt).click();
-               sleep(100).then(() => {
-                    getElementByXpath(buy_node).click();
-                    sleep(500).then(() => {
-                         pwd_ele = document.getElementById("jq_pop_pwd");
-                         pwd_ele.value = pwd;
-                         pwd_ele.dispatchEvent(new Event('input'));
-                         document.getElementById("jq_btn_ok").click();
-                    });
-               });
-          } else if (cnt_ele.className == 'c999') {
+          }
+          else if (cnt_ele.className == 'c999') {
+               location.reload();
+          } else {
+               alert("Unknown!");
                location.reload();
           }
+     },
+     100, function () {
+          getElementByXpath(buy_node).click();
+     },
+     500, function () {
+          pwd_ele = document.getElementById("jq_pop_pwd");
+          pwd_ele.value = pwd;
+          pwd_ele.dispatchEvent(new Event('input'));
+          document.getElementById("jq_btn_ok").click();
+     },
+);
+
+i = 0;
+main_func = function () {
+     if (i >= ary.length - 1) {
+          return;
+     }
+     sleep(ary[i]).then(() => {
+          ary[i + 1]();
+          i += 2;
+          main_func();
      });
-});
+}
+main_func();
