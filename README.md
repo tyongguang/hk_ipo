cd1ad8bc54b6e873b180799dd725fd712f4d21c1
wikotan <wikotan@tencent.com>
2021-02-05 19:54:18 +0800

    更新脚本

--- README.md
+++ README.md
@@ -15,43 +15,13 @@
    
    * 替换上面的123456为自己的密码
    
-   * `cnt`变量中最后的`li[11]`,  `11` 表示认购数量对应的位置号（索引从1开始），看自己的额度适当修改。
+   * `cnt`变量中最后的`li[X]`,  `X` 表示认购数量对应的位置号（索引从1开始），看自己的额度适当修改。
    
-     
-   
-
-###    半自动
-
-1. 将修改好的`vbkr.js`，多行换转换成一行  
-
-   使用`Notepad++` 打开`vbkr.js`， 全部选择，使用快捷键`Ctrl +j`
-
-   
-
-2. 创建或修改书签  
-
-   在chrome浏览器新建一个书签，随便建。编译书签的URL，将URL替为步骤1的内容。
-
-
-
-3. 手动循环刷
-
-    * 进入打新页面，单击上面书签，如果没额度，JS会自动刷新页面，发现页面刷新后，要手动再单击书签，这是没额度或者时间未到的情况，就是需要手动刷。 
-    * 有满足额度，会直接自动交易。
-
-    * 如需要手动确认，只需要将`vbkr.js` 中的`document.getElementById("jq_btn_ok").click();`删掉。
-
-      > 是删掉，千万别注释，在多行变一行的过程中，使用 `//` 注释会坑得怀疑人生。
-
-
-
-### 全自动
-
- 1. Chrome中安装 `Scripty`
+3. Chrome中安装 `Scripty`
 
     > https://chrome.google.com/webstore/detail/scripty-javascript-inject/milkbiaeapddfnpenedfgbfdacpbcbam?hl=en
 
- 2. 使用`Scripty`创建自动脚本
+4. 使用`Scripty`创建自动脚本
 
     **注意**：
     
@@ -61,9 +31,9 @@
     
     ![scripty_vbk](images/scripty_vbkr.png)
 
-3. 打开目标网页
+5. 打开目标网页
 
-4. 躺着
+6. 躺着
 
    
 
--- vbkr.js
+++ vbkr.js
@@ -11,60 +11,74 @@ subscription_node = '//*[@id="iposVue"]/div[2]/div[2]/div[2]/div[1]/ul[1]/li[2]/
 select_node = '//*[@id="iposVue"]/div[2]/div[2]/div[2]/div[1]/ul[1]/li[3]/span[2]/div/div';
 buy_node = '//*[@id="iposVue"]/div[2]/div[2]/div[2]/a';
 
-
-var ary = new Array()
-ary.push(
-     100, function () {
-          subscription_ele = getElementByXpath(subscription_node);
-          if (subscription_ele == null) {
-               location.reload();
-          }
-          subscription_ele.click();
-          return true;
-     },
-     200, function () {
-          getElementByXpath(select_node).click();
-          return true;
-     },
-     300, function () {
-          cnt_ele = getElementByXpath(cnt);
-          if (cnt_ele.className == 'option-item' || cnt_ele.className == 'option-item red') {
-               getElementByXpath(cnt).click();
-          }
-          else if (cnt_ele.className == 'c999') {
-               location.reload();
-               return false;
-          } else {
-               alert("Unknown!");
-               location.reload();
-               return false;
-          }
-          return true;
-     },
-     100, function () {
-          getElementByXpath(buy_node).click();
-          return true;
-     },
-     500, function () {
-          pwd_ele = document.getElementById("jq_pop_pwd");
-          pwd_ele.value = pwd;
-          pwd_ele.dispatchEvent(new Event('input'));
-          document.getElementById("jq_btn_ok").click();
-          return true;
-     },
-);
-
-i = 0;
-main_func = function () {
-     if (i >= ary.length - 1) {
+function main() {
+     first_login_ele = getElementByXpath(login_pw);
+     if (first_login_ele != null) {
+          console.log("need password");
           return;
      }
-     sleep(ary[i]).then(() => {
-          if (false == ary[i + 1]()) {
-              return ;
+
+     ok_ele = getElementByXpath(is_ok);
+     if (ok_ele != null) {
+          console.log("打新已经完成！");
+          return;
+     }
+
+     var ary = new Array()
+     ary.push(
+          100, function () {
+               subscription_ele = getElementByXpath(subscription_node);
+               if (subscription_ele == null) {
+                    location.reload();
+               }
+               subscription_ele.click();
+               return true;
+          },
+          200, function () {
+               getElementByXpath(select_node).click();
+               return true;
+          },
+          300, function () {
+               cnt_ele = getElementByXpath(cnt);
+               if (cnt_ele.className == 'option-item' || cnt_ele.className == 'option-item red') {
+                    getElementByXpath(cnt).click();
+               }
+               else if (cnt_ele.className == 'c999') {
+                    location.reload();
+                    return false;
+               } else {
+                    alert("Unknown!");
+                    location.reload();
+                    return false;
+               }
+               return true;
+          },
+          100, function () {
+               getElementByXpath(buy_node).click();
+               return true;
+          },
+          500, function () {
+               pwd_ele = document.getElementById("jq_pop_pwd");
+               pwd_ele.value = pwd;
+               pwd_ele.dispatchEvent(new Event('input'));
+               document.getElementById("jq_btn_ok").click();
+               return true;
+          },
+     );
+
+     i = 0;
+     main_func = function () {
+          if (i >= ary.length - 1) {
+               return;
           }
-          i += 2;
-          main_func();
-     });
+          sleep(ary[i]).then(() => {
+               if (false == ary[i + 1]()) {
+                    return;
+               }
+               i += 2;
+               main_func();
+          });
+     }
+     main_func();
 }
-main_func();
+main();
