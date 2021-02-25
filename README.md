65f89b65bb92e016978be09d4f568925446b578b
wikotan <wikotan@tencent.com>
2021-02-25 19:53:08 +0800

    更新辉立

--- README.md
+++ README.md
@@ -37,7 +37,49 @@
 
     在未满足条件的情况下，网页会自动刷新，直到抢购成功。
 
-    
+
+
+## 辉立
+
+1. Chrome中安装 `Scripty`
+
+   > https://chrome.google.com/webstore/detail/scripty-javascript-inject/milkbiaeapddfnpenedfgbfdacpbcbam?hl=en
+
+2. 使用`Scripty`创建自动脚本
+
+   打开编辑器打开`phillip.js`，编辑变量：
+
+   * `margin` ： 用数字表示，其意义为：`现金=0，10倍=10，20倍=20，100%=-1 优先20倍，次之10倍=30`
+   * `cnt`: 要打股票数量索引号，“请选择股数" 为0，下面数值顺序增1
+
+   将修改好的`phillip.js`，填入到`Scripty`
+
+   关键参数：
+
+   * match 的URL：`POEMS2/ProductPlatform/LStock/IPO_New/InputIpo.asp`
+   * Trigger: `Automatically`, `On Page Load`
+
+   效果如下：
+
+   ![image-20210220110912888](images/scripty_phillip.png)
+
+3. 打开目标网页
+
+   登录进入到辉立的交易网页后，点击`香港股票`，此时出现下拉菜单，
+
+   鼠标移至`新股认购`，**同时按住`Ctrl`键**,单击，浏览器打开新的网页，我们在这个新的网页上工作。
+
+   > `Ctrl`键进新网页，这一步很关键！！！
+
+   ![](images\scripty_phillip_page.png)
+
+接着就是Next...
+
+当网页进到最终的新股认购网页后，脚本将会自动填入预设的内容并点击`提交`。
+
+当条件不符合要求，将会自动刷新，直到成功。
+
+
 
 ## 华泰
 
@@ -47,11 +89,7 @@
 
 ## 方德
 
-未发现网页版。
+网页版失效
 
 
-
-## 辉立
-
-Todo
 
--- phillip.js
+++ phillip.js
@@ -1,6 +1,6 @@
-javascript:
-pwd = '123456';
-cnt = 10;
+//孖展：现金=0，10倍=10，20倍=20，100%=-1, 优先20倍，次之10倍=30
+margin = 20; 
+cnt = 10; //hand index
 
 function getElementByXpath(path) {
      return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
@@ -9,10 +9,70 @@ function sleep(time) {
      return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-comfirm_node = '//*[@id="3"]/div[2]/form[2]/table/tbody/tr[8]/td/input[1]';
-sleep(200).then(() => {
-    select_ele = document.getElementById("BQty");
-    comfirm_ele = getElementByXpath(comfirm_node);
-    select_ele.selectedIndex  = cnt;
-    comfirm_ele.click();
-});
+function main() {
+     var ary = new Array()
+     ary.push(
+          200, function () {
+               select_ele = document.getElementById("BQty");
+               select_ele.selectedIndex  = cnt;
+               select_ele.dispatchEvent(new Event('change'));
+               return true;
+          },
+          500, function () {
+               OrderSide = document.getElementById("OrderSide");
+               target_index = -100;
+               for(i = 0; i < OrderSide.options.length;i++) {
+                    if (margin == 30) {
+                         if (OrderSide.options[i].text == '95% 孖展') {
+                              target_index = i;
+                              break
+                         }
+                         if (OrderSide.options[i].text == '90% 孖展') {
+                              target_index = i;
+                         }
+                    } else if ((OrderSide.options[i].text == '現金全數' && margin == 0)  ||
+                         (OrderSide.options[i].text == '95% 孖展' && margin == 20)  ||
+                         (OrderSide.options[i].text == '90% 孖展' && margin == 10) ||
+                         (OrderSide.options[i].text == '100% 孖展' && margin == -1)
+                    ) {
+                         target_index = i;
+                    }
+
+               }
+               if (target_index > 0) {
+                    OrderSide.selectedIndex  = target_index;
+                    OrderSide.dispatchEvent(new Event('change'));
+                    return true;
+               }
+
+               console.log("需要的孖展套餐不满足，准备刷新...");
+               location.reload();
+               return false;
+          },
+          200, function () {
+               comfirm_node = '//*[@id="2"]/div[2]/form[2]/table/tbody/tr[7]/td/input[1]';
+               comfirm_ele = getElementByXpath(comfirm_node);
+               console.log("click.....");
+               console.log(comfirm_ele);
+               comfirm_ele.click();
+          }
+     );
+
+     main_idx = 0;
+     main_func = function () {
+          if (main_idx >= ary.length - 1) {
+               return;
+          }
+          sleep(ary[main_idx]).then(() => {
+               if (false == ary[main_idx + 1]()) {
+                    return;
+               }
+               main_idx += 2;
+               main_func();
+          });
+     }
+     main_func();
+}
+main();
+
+
